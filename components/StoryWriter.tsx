a7a401b37483cb2f7ad6889580dae96f296abe20
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-06-25 16:47:09 +0100

    feat: :sparkles: Added gpt script to the build

--- components/StoryWriter.tsx
+++ components/StoryWriter.tsx
@@ -11,9 +11,37 @@ import {
 } from "./ui/select";
 import { Textarea } from "./ui/textarea";
 
+const storiesPath = "/public/stories";
+
 function StoryWriter() {
   const [story, setStory] = useState("");
   const [pages, setPages] = useState<number>();
+  const [progress, setProgress] = useState("");
+  const [runStarted, setRunStarted] = useState<boolean>(false);
+  const [runFinished, setRunFinished] = useState<boolean | null>(null);
+  const [currentTool, setCurrentTool] = useState("");
+
+  async function runScript() {
+    setRunStarted(true);
+    setRunFinished(false);
+
+    const response = await fetch("/api/run-script", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ story, pages, path: storiesPath }),
+    });
+
+    if (response.ok && response.body) {
+      // Handle streams from API
+      console.log("Streaming started");
+    } else {
+      setRunStarted(false);
+      setRunFinished(true);
+      console.error("Failed to start streaming");
+    }
+  }
 
   return (
     <div className="flex flex-col container">
@@ -39,12 +67,52 @@ function StoryWriter() {
           </SelectContent>
         </Select>
 
-        <Button disabled={!story || !pages} className="w-full" size="lg">
+        <Button
+          disabled={!story || !pages || runStarted}
+          className="w-full"
+          size="lg"
+          onClick={runScript}
+        >
           Generate Story
         </Button>
       </section>
 
-      <section className="flex-1 pb-5 mt-5"></section>
+      <section className="flex-1 pb-5 mt-5">
+        <div className="flex flex-col-reverse w-full space-y-2 bg-gray-800 rounded-md text-gray-200 font-mono p-10 h-96 overflow-y-scroll">
+          <div>
+            {runFinished === null && (
+              <>
+                <p className="animate-pulse mr-5">
+                  Im waiting for you to Generate a story above...
+                </p>
+                <br />
+              </>
+            )}
+            <span className="mr-5">{">>"}</span>
+            {progress}
+          </div>
+
+          {/* Current Tool */}
+          {currentTool && (
+            <div className="py-10">
+              <span className="mr-5">{"--- [Current Tool] ---"}</span>
+
+              {currentTool}
+            </div>
+          )}
+
+          {/* Render Events ... */}
+
+          {runStarted && (
+            <div>
+              <span className="mr-5 animate-in">
+                {"--- [AI Storyteller Has Started] ---"}
+              </span>
+              <br />
+            </div>
+          )}
+        </div>
+      </section>
     </div>
   );
 }
