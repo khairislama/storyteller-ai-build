a12580fb1cafecddb7150649015bd316e493afce
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-06-27 16:26:09 +0100

    fix: :bug: Fixed gpt-4o error

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex-1 flex flex-col">
       <section className="flex-1 grid grid-cols-1 lg:grid-cols-2">
         <div className="bg-purple-500 flex flex-col space-y-5 justify-center items-center order-1 lg:-order-1 pb-10">
-          <Image src={Logo} height={250} alt="logo" />
+          <Image src={Logo} height={250} alt="logo" priority />
 
           <Button asChild className="px-20 bg-purple-700 p-10 text-xl">
             <Link href="/stories">Explore Story Library</Link>
